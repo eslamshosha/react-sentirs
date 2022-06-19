b18862fb9989bd8546124ac55d4d42321e22b782
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-06-19 10:48:44 +0200

    first-commit

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar">
+          <a href="#!">
+            <img src=""></img>
+          </a>
+          <div className="input-section">
+            <input type="text"></input>
+          </div>
+        </div>
+        <ul className="big-menu">
+          <li>
+            <Link className="cat-anchor" to="/">Home</Link>
+          </li>
+          <li>
+            <Link className="cat-anchor" to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
